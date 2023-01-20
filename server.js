d283bfc57e00a75536f25779391451e90c44e304
dac <dac@dypm.org>
2023-01-20 17:12:01 +0530

    sdm exam

---
+++ server.js
@@ -0,0 +1,10 @@
+const express=require("express");
+const app=express();
+const path=require("path");
+
+app.get("/",function(req,resp){
+    resp.sendFile(path.join(__dirname,"./public/index"));
+})
+
+app.listen(8000);
+console.log("server is listening on port 8000");
