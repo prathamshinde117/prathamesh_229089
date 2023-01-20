d4f3ae95a46ec280ad35e1050dedebf3660a45a1
Prathamesh Shinde <121031676+prathamshinde117@users.noreply.github.com>
2023-01-20 17:39:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app=express();
 const path=require("path");
 
 app.get("/",function(req,resp){
-    resp.sendFile(path.join(__dirname,"./public/index"));
+    resp.sendFile(path.join(__dirname,"./public/index.html"));
 })
 
 app.listen(8000);
